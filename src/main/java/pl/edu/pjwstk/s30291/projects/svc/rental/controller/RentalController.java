e3cbba611bb7c38bf06176f2cb9ba3f90cad5e21
s30291-pj <s30291@pjwstk.edu.pl>
2025-06-07 16:48:56 +0200

    update

--- src/main/java/pl/edu/pjwstk/s30291/projects/svc/rental/controller/RentalController.java
+++ src/main/java/pl/edu/pjwstk/s30291/projects/svc/rental/controller/RentalController.java
@@ -9,11 +9,27 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import pl.edu.pjwstk.s30291.projects.svc.rental.movie.Movie;
 import pl.edu.pjwstk.s30291.projects.svc.rental.service.RentalService;
 
+@OpenAPIDefinition(
+		info = @Info(
+			title = "Movie Rental Service", 
+			description = "This is a movie rental service API that can be used to fetch movie details or execute rental actions."
+			)
+		)
+
 @RestController
 @RequestMapping("rentals")
+@Tag(name = "Basic Operations - Rental Controller", description = "This controller provides basic rental actions.")
 public class RentalController {
 
 	private RentalService service;
@@ -22,11 +38,29 @@ public class RentalController {
 		this.service = service;
 	}
 	
+	@Operation(summary = "Fetch movie with ID")
+	@ApiResponses(value = { 
+	  @ApiResponse(responseCode = "200", description = "Successfuly fetch movie with provided ID", 
+	    content = { @Content(mediaType = "application/json", 
+	      schema = @Schema(implementation = Movie.class)) }),
+	  @ApiResponse(responseCode = "400", description = "Invalid ID provided", 
+	    content = @Content), 
+	  @ApiResponse(responseCode = "404", description = "Movie with provided ID not found", 
+	    content = @Content) })
 	@GetMapping("/{id}")
 	public ResponseEntity<Movie> getMovie(@PathVariable UUID id) {
 		return ResponseEntity.ok(service.getMovie(id));
 	}
 	
+	@Operation(summary = "Rent movie with ID")
+	@ApiResponses(value = { 
+			  @ApiResponse(responseCode = "200", description = "Movie rental successful", 
+			    content = { @Content(mediaType = "application/json", 
+			      schema = @Schema(implementation = Movie.class)) }),
+			  @ApiResponse(responseCode = "400", description = "Invalid ID/request provided", 
+			    content = @Content), 
+			  @ApiResponse(responseCode = "404", description = "Movie with provided ID not found", 
+			    content = @Content) })
 	@PutMapping("/{id}/rent")
 	public ResponseEntity<Void> rentMovie(@PathVariable UUID id) {
 		service.setAvailability(id, false);
@@ -34,6 +68,15 @@ public class RentalController {
 		return ResponseEntity.ok().build();
 	}
 	
+	@Operation(summary = "Return movie with ID")
+	@ApiResponses(value = { 
+			  @ApiResponse(responseCode = "200", description = "Movie return successful", 
+			    content = { @Content(mediaType = "application/json", 
+			      schema = @Schema(implementation = Movie.class)) }),
+			  @ApiResponse(responseCode = "400", description = "Invalid ID/request provided", 
+			    content = @Content), 
+			  @ApiResponse(responseCode = "404", description = "Movie with provided ID not found", 
+			    content = @Content) })
 	@PutMapping("/{id}/return")
 	public ResponseEntity<Void> returnMovie(@PathVariable UUID id) {
 		service.setAvailability(id, true);
--- src/main/java/pl/edu/pjwstk/s30291/projects/svc/rental/movie/Movie.java
+++ src/main/java/pl/edu/pjwstk/s30291/projects/svc/rental/movie/Movie.java
@@ -3,23 +3,32 @@ package pl.edu.pjwstk.s30291.projects.svc.rental.movie;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "Movie object returned by movie service.")
 public class Movie {
+	@Schema(description = "Unique identifier of the movie.", example = "d997ae27-0a0a-439d-a982-c37279aa0f39")
 	private UUID id;
 	
+	@Schema(description = "Date of movie publication")
 	private LocalDate published;
 	
+	@Schema(description = "Title of the movie")
 	private String title;
 	
+	@Schema(description = "Description of the movie")
 	private String description;
-	 
+	
+	@Schema(description = "Category of the movie")
 	private String category;
 	
+	@Schema(description = "Author of the movie")
 	private String author;
 	
+	@Schema(description = "Availability status of the movie")
 	private boolean available;
 }
